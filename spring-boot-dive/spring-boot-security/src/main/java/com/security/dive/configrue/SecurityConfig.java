ca33ecdd3ac723744af120fbc207a7fa51205b39
姚超臣 <yaochaochen@ctv.org.cn>
2019-11-18 13:06:45 +0800

    spring-boot-security sessionManage

--- spring-boot-dive/spring-boot-security/src/main/java/com/security/dive/configrue/SecurityConfig.java
+++ spring-boot-dive/spring-boot-security/src/main/java/com/security/dive/configrue/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.security.dive.configrue;
 
 import com.security.dive.handler.MyAuthenticationFailureHandler;
 import com.security.dive.handler.MyAuthenticationSucessHandler;
+import com.security.dive.handler.session.MySessionExpiredStrategy;
 import com.security.dive.validate.code.ValidateCodeFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +23,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private ValidateCodeFilter validateCodeFilter;
 
+    @Autowired
+    private MySessionExpiredStrategy sessionExpiredStrategy;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class) //验证码
@@ -30,7 +34,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/login")
                 .successHandler(authenticationSucessHandler)
                 .failureHandler(authenticationFailureHandler)
-                .and().authorizeRequests().antMatchers("\"/authentication/require\",\n" +
-                "                            \"/login.html\", \"/code/image\"").permitAll().anyRequest().authenticated().and().csrf().disable();
+                .and().authorizeRequests().antMatchers("\"/authentication/require\",\n" +//无需认证接口
+                "                            \"/login.html\", \"/code/image\"").permitAll().anyRequest().authenticated()
+                .and().sessionManagement()//添加Session管理器
+                .invalidSessionUrl("/session/invalid")
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true)
+                .expiredSessionStrategy(sessionExpiredStrategy)
+                .and()
+                .and().csrf().disable();
     }
 }
