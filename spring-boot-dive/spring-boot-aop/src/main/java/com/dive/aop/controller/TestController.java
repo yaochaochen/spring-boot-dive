d6afbfba25c22093ca4e71f0506a6d07b60d0cc3
姚超臣 <yaochaochen@ctv.org.cn>
2019-11-19 16:00:10 +0800

    JPA Page

--- spring-boot-dive/spring-boot-aop/src/main/java/com/dive/aop/controller/TestController.java
+++ spring-boot-dive/spring-boot-aop/src/main/java/com/dive/aop/controller/TestController.java
@@ -5,6 +5,11 @@ import com.dive.aop.dao.SysDao;
 import com.dive.aop.dao.impl.SysLogRepository;
 import com.dive.aop.domain.SysLog;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +34,11 @@ public class TestController {
 
     @Log("执行方法三")
     @GetMapping("/three")
-    public void methodThree(String name, String age) {
-
+    public Page<SysLog> methodThree(String name) {
+        Sort sort =Sort.by("username").ascending();
+        Pageable pageable = PageRequest.of(0, 20, sort);
+        Page page = sysDao.findByUsername(name, pageable);
+        page.getContent();
+        return page;
     }
 }
--- spring-boot-dive/spring-boot-aop/src/main/java/com/dive/aop/dao/impl/SysLogRepository.java
+++ spring-boot-dive/spring-boot-aop/src/main/java/com/dive/aop/dao/impl/SysLogRepository.java
@@ -2,9 +2,19 @@ package com.dive.aop.dao.impl;
 
 
 import com.dive.aop.domain.SysLog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SysLogRepository extends CrudRepository<SysLog, Long> {
+
+
+    Page<SysLog> findByUsername(String userName, Pageable pageable);
+
+    Slice<SysLog> findAllByUsername(String userName, Sort sort);
+
 
 
 }
