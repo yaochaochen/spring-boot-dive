f25b84981fe7270c1a2b25e8bc8ddd6b7bfc660f
姚超臣 <yaochaochen@ctv.org.cn>
2019-11-20 15:08:25 +0800

    SpringMVC的内容协商-方法参数

--- spring-boot-dive/spring-boot-aop/src/main/java/com/dive/aop/converter/PropertiesHttpMessageConverter.java
+++ spring-boot-dive/spring-boot-aop/src/main/java/com/dive/aop/converter/PropertiesHttpMessageConverter.java
@@ -20,7 +20,7 @@ import java.util.Properties;
 public class PropertiesHttpMessageConverter extends AbstractGenericHttpMessageConverter<Properties> {
 
 
-    private PropertiesHttpMessageConverter() {
+    public PropertiesHttpMessageConverter() {
         super(new MediaType("text", "properties"));
     }
 
