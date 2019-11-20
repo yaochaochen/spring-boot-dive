4efb3e2e98d46daca50cc787b5154c7a4dbb4d32
姚超臣 <yaochaochen@ctv.org.cn>
2019-11-20 09:45:20 +0800

    JPA Page query

---
+++ spring-boot-dive/spring-boot-CORS-support/src/test/java/com/dive/SpringBootCorsSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCorsSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-boot-dive/spring-boot-jpa/src/test/java/com/dive/demo/SpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dive.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class SpringBootJpaApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
