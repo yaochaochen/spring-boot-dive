df2a7540c5464f1751fa3d60152536423db2c4d9
姚超臣 <yaochaochen@ctv.org.cn>
2019-11-22 16:19:25 +0800

    增加springweb注解

--- spring-boot-dive/spring-webMvc/src/main/java/com/yue/web/controller/HelloWorldController.java
+++ spring-boot-dive/spring-webMvc/src/main/java/com/yue/web/controller/HelloWorldController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloWorldController {
     @RequestMapping("")
     public String index(Model model) {
-        model.addAttribute("message", "Hello World");
+      //  model.addAttribute("acceptLanguage",acceptLanguage);
+        model.addAttribute("message","Hello,World");
         return "index";
     }
 }
