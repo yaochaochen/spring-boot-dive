67832c691a1939e3cb5a300556e02c858eca0ae6
姚超臣 <yaochaochen@ctv.org.cn>
2019-11-22 15:50:50 +0800

    增加springConfigure拦截器

--- spring-boot-dive/spring-webMvc/src/main/java/com/yue/web/conifg/WebMvcConfig.java
+++ spring-boot-dive/spring-webMvc/src/main/java/com/yue/web/conifg/WebMvcConfig.java
@@ -2,17 +2,23 @@ package com.yue.web.conifg;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Spring Mvc配置
  */
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig {
+public class WebMvcConfig  implements WebMvcConfigurer {
 
     @Bean
     public ViewResolver viewResolver() {
@@ -23,5 +29,16 @@ public class WebMvcConfig {
         return viewResolver;
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                System.out.println("拦截中....");
+                return false;
+            }
+        });
+
+    }
 
 }
