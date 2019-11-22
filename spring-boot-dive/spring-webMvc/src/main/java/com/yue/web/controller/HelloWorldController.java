0e1d38556b5a6f51c6cbe6dcbb65c52173637b5f
姚超臣 <yaochaochen@ctv.org.cn>
2019-11-22 15:29:51 +0800

    修改package

--- spring-boot-dive/spring-webMvc/src/main/java/com/yue/web/controller/HelloWorldController.java
+++ spring-boot-dive/spring-webMvc/src/main/java/com/yue/web/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.imooc.web.controller;
+package com.yue.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
