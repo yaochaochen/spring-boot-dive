be0b2bdd41cb4350f2a0f819e1a4b602c12224ba
姚超臣 <yaochaochen@ctv.org.cn>
2019-11-22 15:40:19 +0800

    修改注解SpringMVC

---
+++ spring-boot-dive/spring-webMvc/src/main/java/com/yue/web/conifg/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.yue.web.conifg;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Spring Mvc配置
+ */
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+
+}
