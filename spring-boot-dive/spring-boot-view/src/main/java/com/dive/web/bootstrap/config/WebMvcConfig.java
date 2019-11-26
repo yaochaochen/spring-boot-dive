2d10c21cbcc74ae7cbad9b0dc1a43bb68ad64f54
姚超臣 <yaochaochen@ctv.org.cn>
2019-11-26 16:13:28 +0800

    spring view 增加thymeleaf和JSP共存

---
+++ spring-boot-dive/spring-boot-view/src/main/java/com/dive/web/bootstrap/config/WebMvcConfig.java
@@ -0,0 +1,28 @@
+package com.dive.web.bootstrap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import static org.springframework.core.Ordered.LOWEST_PRECEDENCE;
+
+/**
+ * Spring Mvc配置
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setOrder(LOWEST_PRECEDENCE-5);
+        return viewResolver;
+    }
+
+}
--- spring-boot-dive/spring-boot-view/src/main/java/com/dive/web/bootstrap/controller/HelloWorldController.java
+++ spring-boot-dive/spring-boot-view/src/main/java/com/dive/web/bootstrap/controller/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.dive.web.bootstrap.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
+
+    @RequestMapping("")
+    public String index(@RequestParam(required = false,defaultValue = "0") int value, Model model) {
+        return "index";
+    }
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "hello-world";
